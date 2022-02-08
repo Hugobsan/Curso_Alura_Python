7c4fbeae879d321ef261b4fdfcac7554c4022927
Hugo Barbosa Santos <hugocorone10@gmail.com>
2022-02-08 19:00:54 -0300

    Comparando variáveis

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,11 @@
+print("*********************************")
 print("Bem vindo no jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute = int(input("Digite o seu numero: "))
+
+print("Você digito:", chute)
+
+print("Você acertou!" if numero_secreto == chute else "Você errou")
